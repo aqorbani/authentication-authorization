c615f0e6b302eb9d178588b5fa698d20ce5a82b7
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-19 17:02:47 +0330

    signin redirect

--- pages/signin.js
+++ pages/signin.js
@@ -1,11 +1,22 @@
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const router = useRouter();
+
+  useEffect(() => {
+    fetch("/api/user")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === "success") {
+          // router.replace("/dashboard");
+          window.location.href = "/dashboard";
+        }
+      });
+  }, []);
 
   const signInHandler = async () => {
     const res = await fetch("/api/auth/signin", {
