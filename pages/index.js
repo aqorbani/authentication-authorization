e6c1b43b7ce2ad998b45b9c24129f144fa385cb9
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-19 18:09:46 +0330

    control element by login status

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,40 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    fetch("/api/user")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === "success") {
+          setIsLoggedIn(true);
+        } else {
+          setIsLoggedIn(false);
+        }
+      });
+  }, []);
+
   return (
     <>
-      <button>
-        <Link href="/signup">Sign Up</Link>
-      </button>
-      <button>
-        <Link href="/signin">Sign In</Link>
-      </button>
-      <button>
-        <Link href="/dashboard">dashboard</Link>
-      </button>
+      {isLoggedIn ? (
+        <button>
+          <Link href="/dashboard">dashboard</Link>
+        </button>
+      ) : (
+        <>
+          <button>
+            <Link href="/signup">Sign Up</Link>
+          </button>
+          <button>
+            <Link href="/signin">Sign In</Link>
+          </button>
+        </>
+      )}
     </>
   );
 }
